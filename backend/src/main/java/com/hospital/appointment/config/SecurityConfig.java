0de8761ad9ae7cbb2c2bf6e3b2c684b9e7ad58b0
Dharshan20055 <dharshanmurali05@gmail.com>
2026-03-11 14:57:07 +0530

    Added frontend deployed path

--- backend/src/main/java/com/hospital/appointment/config/SecurityConfig.java
+++ backend/src/main/java/com/hospital/appointment/config/SecurityConfig.java
@@ -60,7 +60,7 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(Arrays.asList("http://localhost:3000"));
+        configuration.setAllowedOrigins(Arrays.asList("http://localhost:3000","https://hospital-appointment-system-individ.vercel.app"));
         configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(Arrays.asList("*"));
         configuration.setAllowCredentials(true);
